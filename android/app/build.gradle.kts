31a3cfcc778107640cc12930442acfa8a4658727
Atl Cardoso <atl1cardoso0@gmail.com>
2025-11-12 17:35:54 -0600

    feat: actualizar configuraciones de Gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,19 +3,16 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.kitsucode"
-    compileSdk = 36
-//    compileSdk = flutter.compileSdkVersion
+    compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -28,8 +25,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = 36
-//        targetSdk = flutter.targetSdkVersion
+        targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
@@ -45,9 +41,4 @@ android {
 
 flutter {
     source = "../.."
-}
-
-dependencies {
-    // --- ¡LÍNEA CORREGIDA! ---
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
